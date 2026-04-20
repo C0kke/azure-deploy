984560c5812da1d0ad2417d46cf6f1d77347a735
Cokke <jorgerandomgamer@gmail.com>
2026-04-20 16:30:23 -0400

    1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { json } from 'stream/consumers';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Jorge Bustos';
   }
-  
-  getNumber(num: number): number {
+
+  getNumber(num: any): number | string {
     const intNumber = Number(num);
+    if (isNaN(intNumber)) {
+      return 'No se ha ingresado un número válido';
+    }
     return (intNumber + 5) * 2;
   }
 
-  getUserData(data: any): string {
+  getUserData(data: any): any {
     const { name, age, n } = data;
-    const result = age / n;
-    return JSON.parse(`{"frase": "Bienvenido ${name}", "calculo": "Tu edad dividida por ${n} es ${result}."}`);
-  }
 
+    const ageIsString = typeof age === 'string';
+    const nIsString = typeof n === 'string';
+    const nIsZero = n === 0 || n === '0';
+
+    if (ageIsString && nIsString) {
+      return { error: 'edad y número n deben ser números' };
+    } else if (ageIsString) {
+      return { error: 'edad debe ser un número' };
+    } else if (nIsString) {
+      return { error: 'número n debe ser un número' };
+    } else if (nIsZero) {
+      return { error: 'el número n no puede ser 0' };
+    }
+
+    const result = age / n;
+    return {
+      frase: `Bienvenido ${name}`,
+      calculo: `Tu edad dividida por ${n} es ${result}.`
+    };
+  }
 }
